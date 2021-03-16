cf399246dca721070a1570ec755e2699a579d006
Kedar Vijay Kulkarni <kedarvijaykulkarni@gmail.com>
2021-03-16 13:48:30 +0530

    feat: add test

--- tests/integration/pods/components/list-video-games-table/component-test.js
+++ tests/integration/pods/components/list-video-games-table/component-test.js
@@ -1,26 +1,64 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | list-video-games-table', function(hooks) {
+module('Integration | Component | list-video-games-table', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.set('game', [
+      {
+        title: 'Hades',
+        genreId: 1,
+        genre: 'RPG',
+        price: 16.79,
+        releaseDate: '2020-09-17',
+        tags: ['Action roguelike', 'roguelike', 'Action', 'indie'],
+      },
+    ]);
 
-    await render(hbs`<ListVideoGamesTable />`);
+    this.set('clicked', false);
 
-    assert.equal(this.element.textContent.trim(), '');
+    this.set('test', () => {
+      this.set('clicked', true);
+    });
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <ListVideoGamesTable>
-        template block text
-      </ListVideoGamesTable>
-    `);
+  test('it renders and table row set the values', async function (assert) {
+    await render(hbs`<ListVideoGamesTable 
+        @data={{game}}
+        deleteRecord={{action test}}
+      />`);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(
+      find('td[data-test="td-title"]').innerText,
+      this.game[0].title,
+      'Title is set correctly'
+    );
+
+    assert.equal(
+      find('td[data-test="td-genre"]').innerText,
+      this.game[0].genre,
+      'Genre is set correctly'
+    );
+
+    assert.equal(
+      find('td[data-test="td-tags"] > span.tag').innerText,
+      this.game[0].tags[0],
+      'Tags is set correctly'
+    );
+
+    assert.equal(
+      find('td[data-test="td-release-date"]').innerText,
+      '17-09-2020',
+      'Release date is set correctly'
+    );
+
+    assert.equal(
+      find('td[data-test="td-price"] > div').innerText,
+      '16,79€',
+      'Genre is set correctly'
+    );
   });
 });
